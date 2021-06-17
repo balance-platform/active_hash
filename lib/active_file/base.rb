56563a8babf429f7574dbdb3b97c8389ee348ce0
simple-lad <sablinan@gmail.com>
2021-06-17 13:51:26 +0300

    Update base.rb

--- lib/active_file/base.rb
+++ lib/active_file/base.rb
@@ -2,6 +2,7 @@ module ActiveFile
 
   class Base < ActiveHash::Base
     extend ActiveFile::MultipleFiles
+    @@instance_lock = Mutex.new
 
     class_attribute :filename, :root_path, :data_loaded, instance_reader: false, instance_writer: false
 
@@ -13,10 +14,12 @@ module ActiveFile
       end
 
       def reload(force = false)
-        return if !self.dirty && !force && self.data_loaded
-        self.data_loaded = true
-        self.data = load_file
-        mark_clean
+        @@instance_lock.synchronize do
+          return if !self.dirty && !force && self.data_loaded
+          self.data = load_file
+          mark_clean
+          self.data_loaded = true
+        end
       end
 
       def set_filename(name)
@@ -34,6 +37,11 @@ module ActiveFile
       def full_path
         actual_filename  = filename   || name.tableize
         File.join(actual_root_path, "#{actual_filename}.#{extension}")
+      end
+      
+      def all_in_process
+        return super if data_loaded
+        @records || []
       end
 
       def extension
